ff8b92751bc25a1b8a0b8e864a459c01cd3ee25e
Logical Contradiction <chrisgiaramita@yahoo.com>
2025-06-26 22:32:50 -0400

    Created Models folder

--- Models/Track.cs
+++ Models/Track.cs
@@ -4,23 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicDbEditor
+namespace MusicDbEditor.Models
 {
     /// <summary>
     /// Represents info needed to display in Track tab
     /// </summary>
     class Track
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string NameInStreamPlayer { get; set; }
-        public string AlbumName { get; set; }
-        public string Link { get; set; }
-        public string Notes { get; set; }
-        public string SourceMediaName { get; set; }
+
+        #region Properties
 
         /// <summary>
-        /// Constructs a Track object.
+        /// The id of this track from the database
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of this Track
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The name in the steam player for this track
+        /// </summary>
+        public string NameInStreamPlayer { get; set; }
+
+        /// <summary>
+        /// The name of the album this track is from
+        /// </summary>
+        public string AlbumName { get; set; }
+
+        /// <summary>
+        /// A link to listen to this track at.
+        /// </summary>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Arbitrary text the user decided was important enough to write down
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// The name of the piece of media this track originates from
+        /// </summary>
+        public string SourceMediaName { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Constructs a Track object with given parameters.
         /// </summary>
         /// <param name="id">The primary key from the record.</param>
         /// <param name="name">The name of the track.</param>
@@ -29,9 +61,9 @@ namespace MusicDbEditor
         /// <param name="link">A link to listen to the track.</param>
         /// <param name="notes">The notes about a track.</param>
         /// <param name="sourceMediaName">The name of the source media this track is from.</param>
-        public Track(int id, string name, string nameInStreamPlayer, string albumName, 
-            string link, string notes, string sourceMediaName) 
-        { 
+        public Track(int id, string name, string nameInStreamPlayer, string albumName,
+            string link, string notes, string sourceMediaName)
+        {
             Id = id;
             Name = name;
             NameInStreamPlayer = nameInStreamPlayer;
@@ -40,6 +72,11 @@ namespace MusicDbEditor
             Notes = notes;
             SourceMediaName = sourceMediaName;
         }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Track() { }
 
 
     }
