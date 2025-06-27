f109c4184fb4fd71286eb70f6bcbeccd61d7cb47
Logical Contradiction <chrisgiaramita@yahoo.com>
2025-06-26 22:39:00 -0400

    data object for source media

---
+++ Models/SourceMedia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicDbEditor.Models
+{
+    internal class SourceMedia
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SortName { get; set; }
+
+
+        public SourceMedia(int id, string name, string sortName)
+        {
+            Id = id;
+            Name = name;
+            SortName = sortName;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Source Media: id='{Id}', name='{Name}', sort name='{SortName}'";
+        }
+
+    }
+}
