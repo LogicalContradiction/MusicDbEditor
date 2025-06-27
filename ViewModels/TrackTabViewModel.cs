5df801e4c15188cb9eee5f0b60446038061fb0bf
Logical Contradiction <chrisgiaramita@yahoo.com>
2025-06-26 22:35:31 -0400

    ViewModels for TrackTab and Track

---
+++ ViewModels/TrackTabViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MusicDbEditor.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MusicDbEditor.ViewModels
+{
+    internal class TrackTabViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The collection of tracks shown to the user.
+        /// </summary>
+        public ObservableCollection<TrackViewModel> Tracks { get; set; }
+
+        public ICommand GetTrackDataCommand { get; set; }
+
+
+
+
+
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public TrackTabViewModel()
+        {
+            // Get the track data from the db and turn them into viewmodels
+            var data = DataConnection.Instance.GetTrackData().Select(track => new TrackViewModel(track));
+
+            // Put them in this viewmodel
+            this.Tracks = new ObservableCollection<TrackViewModel>(data);
+
+            
+        }
+    }
+}
---
+++ ViewModels/TrackViewModel.cs
@@ -0,0 +1,119 @@
+﻿using MusicDbEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicDbEditor.ViewModels
+{
+    internal class TrackViewModel : BaseViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// The Track data object.
+        /// </summary>
+        public Track Track { get; set; }
+
+        /// <summary>
+        /// The name of this Track
+        /// </summary>
+        public string Name {
+            get
+            {
+                return Track.Name;
+            }
+            set {
+                Track.Name = value;
+            }
+        }
+
+        /// <summary>
+        /// The name in the steam player for this track
+        /// </summary>
+        public string NameInStreamPlayer
+        {
+            get
+            {
+                return Track.NameInStreamPlayer;
+            }
+            set
+            {
+                Track.NameInStreamPlayer = value;
+            }
+        }
+
+        /// <summary>
+        /// The name of the album this track is from
+        /// </summary>
+        public string AlbumName
+        {
+            get
+            {
+                return Track.AlbumName;
+            }
+            set
+            {
+                Track.AlbumName = value;
+            }
+        }
+
+        /// <summary>
+        /// A link to listen to this track at.
+        /// </summary>
+        public string Link
+        {
+            get
+            {
+                return Track.Link;
+            }
+            set
+            {
+                Track.Link = value;
+            }
+        }
+
+        /// <summary>
+        /// Arbitrary text the user decided was important enough to write down
+        /// </summary>
+        public string Notes
+        {
+            get
+            {
+                return Track.Notes;
+            }
+            set
+            {
+                Track.Notes = value;
+            }
+        }
+
+        /// <summary>
+        /// The name of the piece of media this track originates from
+        /// </summary>
+        public string SourceMediaName
+        {
+            get
+            {
+                return Track.SourceMediaName;
+            }
+            set
+            {
+                Track.SourceMediaName = value;
+            }
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="track">The track data object this view model represents.</param>
+        public TrackViewModel(Track track)
+        {
+            Track = track;
+        }
+    }
+}
