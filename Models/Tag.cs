6650d5b87af5221f8e2f2254a183a2e1a2d0ffe1
Logical Contradiction <chrisgiaramita@yahoo.com>
2025-09-05 11:52:41 -0400

    Added Tag data object and view model.

---
+++ Models/Tag.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace MusicDbEditor.Models
+{
+    /// <summary>
+    /// Represents a piece of data used to label more data
+    /// </summary>
+    public class Tag
+    {
+        #region Properties
+
+        /// <summary>
+        /// Database-assigned number used to uniquely identify this tag
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The text that represents this label.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The name of the category this tag belongs to.
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// String describing what the tag represents.
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Tag() { }
+
+        #endregion
+
+    }
+}
---
+++ ViewModels/DataViewModels/TagViewModel.cs
@@ -0,0 +1,84 @@
+﻿using MusicDbEditor.Models;
+using System.ComponentModel;
+
+namespace MusicDbEditor.ViewModels.DataViewModels
+{
+    class TagViewModel : INotifyPropertyChanged
+    {
+        #region Interface Methods
+
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The Tag object this viewmodel represents
+        /// </summary>
+        private Tag Tag {  get; set; }
+
+        /// <summary>
+        /// The text representing this label.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return Tag.Name;
+            }
+            set
+            {
+                Tag.Name = value;
+            }
+        }
+
+        /// <summary>
+        /// The category this tag is a member of.
+        /// </summary>
+        public string Category
+        {
+            get
+            {
+                return Tag.Category;
+            }
+            set
+            {
+                Tag.Category = value;
+            }
+        }
+
+        /// <summary>
+        /// Text description of the tag.
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return Tag.Description;
+            }
+            set
+            {
+                Tag.Description = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor. Creates a view model to represent the supplied Tag.
+        /// </summary>
+        /// <param name="tag">Tag object to be represented by this view model.</param>
+        public TagViewModel(Tag tag)
+        {
+            Tag = tag;
+        }
+
+        #endregion
+
+
+
+    }
+}
